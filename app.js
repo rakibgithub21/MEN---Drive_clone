06704ded27780bf2095e54bda6290dc904be6a89
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-11-18 16:34:58 +0600

    make a simple home page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const connectToDb = require('./config/db')
 connectToDb()
 
 const userRouter = require('./routes/user.routes');
+const indexRouter = require('./routes/index.routes')
 
 //for ejs setup
 app.set('view engine', 'ejs') // then create views folder in root
@@ -16,7 +17,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
-app.use('/user/v1',userRouter)
+app.use('/user/v1', userRouter)
+app.use('/',indexRouter)
 
 
 app.listen(3000, () => {
---
+++ routes/index.routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/home', (req, res) => {
+    res.render('home')
+})
+
+
+
+
+
+
+
+
+
+
+module.exports = router
